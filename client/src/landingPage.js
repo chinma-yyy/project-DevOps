f88dd5eae6f240425192fd2bdadcd293a7e5fe64
Chinmay Shewale <shewalechinmay54@gmail.com>
2023-09-30 17:44:28 +0530

    format

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -3,12 +3,14 @@
 import React from 'react';
 
 const LandingPage = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to the Landing Page</h1>
-      <button onClick={() => window.location.href = '/todos'}>Go to Todos</button>
-    </div>
-  );
+	return (
+		<div className="landing">
+			<h1>Welcome to the Landing Page</h1>
+			<button onClick={() => (window.location.href = '/todos')}>
+				Go to Todos
+			</button>
+		</div>
+	);
 };
 
 export default LandingPage;
